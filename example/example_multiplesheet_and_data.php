970e5e97dc1fce746f59cb618416f7ec50c87f59
Amey Damle <amey.damle@plus91.in>
2016-12-13 17:07:36 +0530

    change in example script to shwo better example

--- example/example_multiplesheet_and_data.php
+++ example/example_multiplesheet_and_data.php
@@ -12,23 +12,23 @@
 	for($iKey=0;$iKey < $iSheet;$iKey++){
 		$objWork->setActiveSheetOfExcel($iKey);
 		// Use only for multiple sheets..
-		$objWork->setInitialRowValue();
 		$objWork->setColumnWidth(20);
 		
-		$objWork->setHeaderTextFirst("This is a Example sheet with dummy data");
+		$objWork->setHeaderTextFirst("This is a {$iSheet} Example sheet with dummy data");
 
 		$aHeaders=array('Sr No','Name','State','City','Pin Code','Mobile No'); 
-		$objWork->setHeaderNames($aHeaders);
+		//$objWork->setHeaderNames($aHeaders);
 	
-	$aExportData=array(array('1','JOHN','UK','London','411028','1234567890'),
-				array('2','mark','US','NY','411028','123456789'),
-				array('3','Petert','Brasil','Parogue','493661','0123456789'),
-				array('4','Michel','','123456','493661',''),
-				array('5','Abdul','Pakistan','karachi','411028','0123456789'),
-			);
-	
-	$objWork->setDataToExcel($aExportData);
+		$aExportData=array(array('1','JOHN','UK','London','411028','1234567890'),
+					array('2','mark','US','NY','411028','123456789'),
+					array('3','Petert','Brasil','Parogue','493661','0123456789'),
+					array('4','Michel','','123456','493661',''),
+					array('5','Abdul','Pakistan','karachi','411028','0123456789'),
+				);
+		$objWork->setDataAndColumnToExcel($aHeaders,$aExportData);
+		//$objWork->setDataToExcel($aExportData);
 
+		$objWork->setHeaderTextAtEnd("This is a Our footer of Excel sheet..");
 	}
 
 	
@@ -46,6 +46,5 @@
 	$objWork->setExcelFilename("MyFirstExcel");
 	$objWork->setExcelFileExtension('xlsx'); // by default xls format
 	$objWork->setActiveSheetOfExcel(0);
-	$objWork->setHeaderTextAtEnd("This is a Our footer of Excel sheet..");
 	$objWork->exportFile();
 ?>
